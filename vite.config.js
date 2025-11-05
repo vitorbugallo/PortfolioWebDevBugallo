b4d42352ff836aea701fb4669670e80d71bf50d1
vitorbugallo <vitorbugallo@hotmail.com>
2025-11-04 21:52:57 -0300

    fix:corrige config do Vite para assets e deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   server: {
     port: 5173
-  }
+  },
+  base: '/',       
+  publicDir: 'public' 
 })
